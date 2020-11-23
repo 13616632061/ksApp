f24bcb7b7918b51259eb0506c53af63765a4909e
李云飞（80004885） <80004885@sfmail.sf-express.com>
2020-11-23 19:05:04 +0800

    【优化】1.搭建项目组件化架构 【优化】2.新增商家平台组件化 【优化】3.升级项目3.5.2AS上运行

---
+++ component-shoper-platform/src/main/java/com/example/component_shoper_platform/p/ShopperPlatformFragmentPresenter.kt
@@ -0,0 +1,7 @@
+package com.example.component_shoper_platform.p
+
+import com.example.component_shoper_platform.view.fragment.ShopperPlatformFragment
+import com.library.base.mvp.BasePresenter
+
+class ShopperPlatformFragmentPresenter(mView: ShopperPlatformFragment) : BasePresenter<ShopperPlatformFragment>(mView) {
+}
---
+++ component-shoper-platform/src/main/java/com/example/component_shoper_platform/view/activity/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.component_shoper_platform.view.activity;
+
+
+import com.example.component_shoper_platform.R;
+import com.library.base.mvp.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+
+    @Override
+    public int getContentView() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+}
